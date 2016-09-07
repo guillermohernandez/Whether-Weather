771887325c6401e767d423556e1f9db99d9a35ef
Guillermo Hernandez <guillermohernandez@gmail.com>
2016-09-07 04:11:51 -0500

    code janitor activate

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,10 @@
 'use strict';
 
-$(document).ready(function() {
+$(document).ready(function () {
     $.ajax({
         url: "http://api.wunderground.com/api/714a33a55874728b/geolookup/conditions/q/TX/Austin.json",
         dataType: "jsonp",
-        success: function(parsed_json) {
+        success: function (parsed_json) {
             var location = parsed_json['location']['city'];
             var temp_f = parsed_json['current_observation']['temp_f'];
             // var icon_url = parsed_json['current_observation']['icon_url'];
@@ -14,17 +14,17 @@ $(document).ready(function() {
     });
 });
 
-function greenBeltLevels(){
+function greenBeltLevels() {
     //use  http://waterservices.usgs.gov/
     $.ajax({
         url: "http://waterservices.usgs.gov/nwis/dv/?format=json&sites=08155300",
-        success: function(){
-            
+        success: function () {
+
         }
     })
 };
 
 
-function poolHours(){
+function poolHours() {
     //get pool hours from City API if possible
 }
